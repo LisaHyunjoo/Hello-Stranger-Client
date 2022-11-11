a0956aa0b0d07ffb0178a9dd53f50bb94acca0b3
Lisa <lisahongofficial@gmail.com>
2022-11-11 15:24:15 -0500

    user login is working

--- hello-stranger-client/src/App.js
+++ hello-stranger-client/src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import React from 'react'
-import {Route, Routes, useNavigate} from 'react-router-dom'
+import {Route, Routes, useNavigate} from 'react-router-dom';
 import NavBar from './component/NavBar';
 import Home from './component/Home';
 import RegisterUser from './component/RegisterUser';
+import LoginUser from './component/LoginUser';
 
-let baseUrl = 'http://localhost:8000'
+let baseUrl = 'http://localhost:8000/hellostranger'
 
 export default function App() {
-  // const navigate = useNavigate()
-
+  const navigate = useNavigate()
 
   const register = async(e) => {
     e.preventDefault()
     console.log(e.target)
-    const url = baseUrl + '/hellostranger/user/register'
+    const url = baseUrl + '/user/register'
     try {
       const response = await fetch(url, {
         method:'POST',
@@ -30,7 +30,7 @@ export default function App() {
       console.log(response)
       if(response.status === 201) {
         console.log('register is working')
-        // navigate("login")
+        navigate("/login")
       }
     }
     catch (err) {
@@ -38,12 +38,45 @@ export default function App() {
     }
   }
 
+  const login = async (e) => {
+    console.log('loginUser')
+    console.log(e.target.email.value)
+    e.preventDefault()
+    const url = baseUrl + '/user/login'
+    const loginBody = {
+      email: e.target.email.value,
+      password: e.target.password.value
+    }
+
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          body: JSON.stringify(loginBody),
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          credentials: "include"
+        })
+  
+        console.log(response)
+        console.log("BODY: ",response.body)
+  
+        if (response.status === 200) {
+          navigate("/")
+        }
+      }
+      catch (err) {
+        console.log('Error => ', err);
+      }
+    }
+
   return (
     <>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/register" element={<RegisterUser register={register}/>}/>
+        <Route path="/login" element={<LoginUser login={login}/>}/>
       </Routes>
     </>
   )
--- hello-stranger-client/src/component/NavBar.jsx
+++ hello-stranger-client/src/component/NavBar.jsx
@@ -4,14 +4,9 @@ import {NavLink, Link} from 'react-router-dom'
 export default function NavBar() {
     return(
         <nav>
-            <li>Home</li>
-            <li>Register</li>
-            <li>Login</li>
-            <li>Posts</li>
-
-            {/* <NavLink to="register">Register</NavLink>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="register">Register</NavLink>
             <NavLink to="login">Log In</NavLink>
-            <NavLink to="posts">Posts</NavLink> */}
         </nav>
     )
 }
