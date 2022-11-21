bc3b5c6c48e181f1312ca142e55897dc0cbd412c
Lisa <lisahongofficial@gmail.com>
2022-11-20 20:23:57 -0500

    delete json file

--- src/component/WritePost.jsx
+++ src/component/WritePost.jsx
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react'
-import CountryStateCity from './CountryStateCity.json'
 
 const WritePost = (props) => {
     const [post, setPost] = useState({
@@ -32,15 +31,6 @@ const WritePost = (props) => {
             <label htmlFor='title'>Title: </label>
             <input id="title" type="text" value={post.title} onChange={handleChange}/>
             <br/>
-            {/* <label>Country</label> */}
-            {/* <select name="country">
-                <option value="">--Select Country--</option>
-                {
-                    CountryStateCity.map((country)=> (
-                        <option key={country.country_id} value={country.country._name}>{country.country_name}</option>
-                    ))
-                }
-            </select> */}
             <label htmlFor='content'>Content: </label>
             <textarea id="content" type="text" value={post.content} onChange={handleChange}></textarea>
             <br/>
